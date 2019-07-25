82445fe79accad8d01e96d92163f22e03ac2fb76
Alexey Korolev <alphatroya@gmail.com>
2019-07-25 14:22:13 +0300

    Add input hours validation

--- responses.go
+++ responses.go
@@ -1,13 +1,19 @@
 package main
 
+import "fmt"
+
 const (
-	WrongTokenMessageResponse          = "Неправильное количество аргументов"
-	SuccessTokenMessageResponse        = "Токен успешно обновлен"
-	WrongHostMessageResponse           = "Неправильное количество аргументов"
-	SuccessHostMessageResponse         = "Адрес сервера успешно обновлен"
-	SuccessFillHoursMessageResponse    = "В задачу %s добавлено часов: %s 🎉"
-	WrongFillHoursTokenNilResponse     = "Токен доступа для текущего пользователя не найден"
-	WrongFillHoursHostNilResponse      = "Адрес сервера не найден"
-	WrongFillHoursWrongIssueIdResponse = "Введен неправильный номер задачи"
-	UnknownCommandResponse             = "Введена неправильная команда"
+	WrongTokenMessageResponse             = "Неправильное количество аргументов"
+	SuccessTokenMessageResponse           = "Токен успешно обновлен"
+	WrongHostMessageResponse              = "Неправильное количество аргументов"
+	SuccessHostMessageResponse            = "Адрес сервера успешно обновлен"
+	WrongFillHoursTokenNilResponse        = "Токен доступа для текущего пользователя не найден"
+	WrongFillHoursHostNilResponse         = "Адрес сервера не найден"
+	WrongFillHoursWrongIssueIdResponse    = "Введен неправильный номер задачи"
+	WrongFillHoursWrongHoursCountResponse = "Введено неправильное количество часов"
+	UnknownCommandResponse                = "Введена неправильная команда"
 )
+
+func SuccessFillHoursMessageResponse(issueId string, hours string, host string) string {
+	return fmt.Sprintf("В задачу %s добавлено часов: %s (%s/issues/%s/time_entries)", issueId, hours, host, issueId)
+}
