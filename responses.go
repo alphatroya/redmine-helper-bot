14aaf8d23da5030c76d5e93b68967264eb733782
Alexey Korolev <alphatroya@gmail.com>
2019-07-25 13:43:03 +0300

    Add issue id number check (#5)

--- responses.go
+++ responses.go
@@ -1,12 +1,13 @@
 package main
 
 const (
-	WrongTokenMessageResponse       = "Неправильное количество аргументов"
-	SuccessTokenMessageResponse     = "Токен успешно обновлен"
-	WrongHostMessageResponse        = "Неправильное количество аргументов"
-	SuccessHostMessageResponse      = "Адрес сервера успешно обновлен"
-	SuccessFillHoursMessageResponse = "В задачу %s добавлено часов: %s 🎉"
-	WrongFillHoursTokenNilResponse  = "Токен доступа для текущего пользователя не найден"
-	WrongFillHoursHostNilResponse   = "Адрес сервера не найден"
-	UnknownCommandResponse          = "Введена неправильная команда"
+	WrongTokenMessageResponse          = "Неправильное количество аргументов"
+	SuccessTokenMessageResponse        = "Токен успешно обновлен"
+	WrongHostMessageResponse           = "Неправильное количество аргументов"
+	SuccessHostMessageResponse         = "Адрес сервера успешно обновлен"
+	SuccessFillHoursMessageResponse    = "В задачу %s добавлено часов: %s 🎉"
+	WrongFillHoursTokenNilResponse     = "Токен доступа для текущего пользователя не найден"
+	WrongFillHoursHostNilResponse      = "Адрес сервера не найден"
+	WrongFillHoursWrongIssueIdResponse = "Введен неправильный номер задачи"
+	UnknownCommandResponse             = "Введена неправильная команда"
 )
