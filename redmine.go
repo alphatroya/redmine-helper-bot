90310a4e1be18a1c049c1029756cf36d756245d9
Alexey Korolev <alphatroya@gmail.com>
2019-07-31 12:27:14 +0300

    Fix linter warnings

--- redmine.go
+++ redmine.go
@@ -38,14 +38,14 @@ func FillHoursRequest(token string, host string, message []string, client HTTPCl
 	request.Header.Set("X-Redmine-API-Key", token)
 	request.Header.Set("Content-Type", "application/json")
 	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
 	defer response.Body.Close()
 
 	if response.StatusCode >= 400 {
 		return nil, fmt.Errorf("Wrong response from redmine server %d - %s", response.StatusCode, http.StatusText(response.StatusCode))
-	}
-
-	if err != nil {
-		return nil, err
 	}
 
 	return requestBody, nil
