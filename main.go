cfaa673b7b0aa007d1ac18175e154ffb1b53dec1
Alexey Korolev <alphatroya@gmail.com>
2019-08-08 20:04:48 +0300

    Replace logics by webhook

--- main.go
+++ main.go
@@ -30,18 +30,23 @@ func main() {
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	updateConfig := tgbotapi.NewUpdate(0)
-	updateConfig.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(updateConfig)
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook("https://alphatroya-telegram-bot.herokuapp.com/"+bot.Token))
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe("0.0.0.0:8443", nil)
 
 	clientManager := redmine.NewClientManager(&http.Client{})
-
 	handler := UpdateHandler{bot, redisClient, clientManager}
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
