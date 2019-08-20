31e5c96601643de019bdd8d41d9894497afff9e0
Alexey Korolev <alphatroya@gmail.com>
2019-08-20 16:32:33 +0300

    Improve intro message

--- commands/intro.go
+++ commands/intro.go
@@ -8,7 +8,18 @@ func newIntroCommand() *Intro {
 }
 
 func (i Intro) Handle(message string) (*CommandResult, error) {
-	return NewCommandResult(`Бот активирован, перед использованием необходимо указать адрес сервера и авторизационный токен`), nil
+	return NewCommandResult(`
+*Бот активирован*
+
+Перед тем как его использовать, необходимо установить: 
+
+1. адрес сервера c помощью команды /host <адрес сервера>
+2. ключ доступа к API c помощью команды /token <API токен>
+
+Ключ доступа к API можно найти в боковом меню на экране настроек профиля в Redmine.
+
+Заполнение часов осуществляется с помощью команды /fillhours
+`), nil
 }
 
 func (i Intro) IsCompleted() bool {
