21f46ffc6ab4cc8b3d916ed36b6d3fa101aaedc4
Alexey Korolev <alphatroya@gmail.com>
2019-08-20 21:09:36 +0300

    Cover Host and Token classes by unit tests

--- commands/command.go
+++ commands/command.go
@@ -13,10 +13,6 @@ func (c *CommandResult) Message() string {
 	return c.message
 }
 
-func (c *CommandResult) SetMessage(message string) {
-	c.message = message
-}
-
 func NewCommandResult(message string) *CommandResult {
 	return &CommandResult{message: message}
 }
