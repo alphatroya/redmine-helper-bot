244c1e177ff000a6d2f788aea5999d5b7f938ffa
Alexey Korolev <alphatroya@gmail.com>
2019-12-09 17:28:51 +0300

    Begin activities command rewriting

--- commands/activities.go
+++ commands/activities.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/alphatroya/redmine-helper-bot/redmine"
@@ -13,13 +15,35 @@ type Activities struct {
 	redmineClient redmine.Client
 	storage       storage.Manager
 	chatID        int64
+	activities    []*redmine.Activities
+	completed     bool
 }
 
 func newActivitiesCommand(redmineClient redmine.Client, storage storage.Manager, chatID int64) *Activities {
 	return &Activities{redmineClient: redmineClient, storage: storage, chatID: chatID}
 }
 
-func (a Activities) Handle(message string) (*CommandResult, error) {
+func (a *Activities) Handle(message string) (*CommandResult, error) {
+	if len(a.activities) == 0 {
+		return a.firstPhase()
+	}
+	activityID := string(regexp.MustCompile(`^[0-9]+`).Find([]byte(message)))
+	errorMessage := "Вы ввели неправильный номер активности"
+	if len(activityID) == 0 {
+		return nil, errors.New(errorMessage)
+	}
+
+	for _, activity := range a.activities {
+		if fmt.Sprintf("%d", activity.Id) == activityID {
+			a.storage.SetActivity(activityID, a.chatID)
+			a.completed = true
+			return NewCommandResult("Активность по умолчанию успешно сохранена. Она будет использоваться при каждой команде заполнения часов"), nil
+		}
+	}
+	return nil, errors.New(errorMessage)
+}
+
+func (a *Activities) firstPhase() (*CommandResult, error) {
 	activities, err := a.redmineClient.Activities()
 	if err != nil {
 		return nil, err
@@ -29,17 +53,24 @@ func (a Activities) Handle(message string) (*CommandResult, error) {
 	table := tablewriter.NewWriter(tableString)
 	table.SetHeader([]string{"ID", "Название"})
 
+	var buttons []string
 	for _, activity := range activities {
+		activityID := fmt.Sprintf("%d", activity.Id)
 		data := []string{
-			fmt.Sprintf("%d", activity.Id),
+			activityID,
 			wrap(activity.Name, 20),
 		}
+		buttons = append(buttons, activityID+" - "+activity.Name)
 		table.Append(data)
 	}
 	table.Render()
-	return NewCommandResult("`" + tableString.String() + "`"), nil
+	a.activities = activities
+	return NewCommandResultWithMessagesAndKeyboard([]string{
+		"`" + tableString.String() + "`",
+		"_Введите номер новой активности по умолчанию_",
+	}, buttons), nil
 }
 
-func (a Activities) IsCompleted() bool {
-	return true
+func (a *Activities) IsCompleted() bool {
+	return a.completed
 }
