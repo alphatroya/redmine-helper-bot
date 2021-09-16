648abf8efb759e3e08127dc40f0a1a0a3b8c8a52
Alexey Korolev <alphatroya@gmail.com>
2021-09-16 15:32:21 +0300

    Fix tests

--- commands/commands_builder_test.go
+++ commands/commands_builder_test.go
@@ -27,7 +27,6 @@ func TestBotCommandsBuilder_Build(t *testing.T) {
 		{"*commands.FillHoursMany", "", "fhm"},
 		{"*commands.AddComment", "", "comment"},
 		{"*commands.AddComment", "", "reject"},
-		{"*commands.MyIssues", "", "ls"},
 	}
 	for _, input := range checkData {
 		mockStorage := storage.NewStorageMock()
