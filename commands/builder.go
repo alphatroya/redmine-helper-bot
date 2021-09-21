d3ce333f889f0e2efc94fe21480eda62737d77bc
Alexey Korolev <alphatroya@gmail.com>
2021-09-21 16:12:52 +0300

    Add empty string command

--- commands/builder.go
+++ commands/builder.go
@@ -46,6 +46,8 @@ func (b BotCommandsBuilder) Build(command string, message string, chatID int64) 
 	case "fstatus":
 		redmineClient := redmine.NewClientManager(&http.Client{}, b.storage, chatID)
 		return NewFillStatus(redmineClient, chatID)
+	case "":
+		return newIntroCommand()
 	default:
 		return NewUnknownCommand()
 	}
